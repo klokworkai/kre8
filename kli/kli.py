cdb674bfc280214a49bc7ad19c55b198b62cae89
John M Jeyakumar <kre8new@gmail.com>
2026-02-27 21:08:12 -0500

    added kli folder

---
+++ kli/kli.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+def main():
+    intent = " ".join(sys.argv[1:])
+    response = requests.post("http://localhost:8080/plan", json={"intent": intent})
+    print(response.json())
+
+if __name__ == "__main__":
+    main()
